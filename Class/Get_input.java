8c918fb8a4bd5810a4f26991e85ce73a0f806ad3
alaminhossainrifat <116902132+alaminhossainrifat@users.noreply.github.com>
2023-01-20 22:25:13 +0600

    Add files via upload

--- Class/Get_input.java
+++ Class/Get_input.java
@@ -17,17 +17,32 @@ public class Get_input {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        
         int number;
 
 
         System.out.print("Enter Any Number: ");
         number = input.nextInt();
-
-
         System.out.println("Number ="+number);
+
+
+        
+
+        /* String Input
+         * 
+         * 
+         * 
+         * 
+        
+        Scanner input = new Scanner(System.in);
+        String name;
+        System.out.println("Enter Your Name: ");
+        name = input.nextLine();
+        System.out.println("Welcome: "+name);
+
+
+
+        
+         */
     }
 }
-
-
-
-// 07.31s
